360b21f0465eccae0c04b98f22be71b28cea21bd
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-09-05 01:10:02 +0300

    Initial commit

---
+++ AppTheMovieDB/AppTheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTheMovieDBApp.swift
+//  AppTheMovieDB
+//
+//  Created by Вячеслав Гудзенко on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppTheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
