d382393dc87e34726099edcc65637c7ab33fff82
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-09-05 20:04:13 +0300

    start of work on MovieFetcher

---
+++ AppTheMovieDB/API/MovieFetcher.swift
@@ -0,0 +1,20 @@
+//
+//  MovieFetcher.swift
+//  AppTheMovieDB
+//
+//  Created by Вячеслав Гудзенко on 05.09.2022.
+//
+
+import SwiftUI
+
+struct MovieFetcher: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieFetcher_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieFetcher()
+    }
+}
