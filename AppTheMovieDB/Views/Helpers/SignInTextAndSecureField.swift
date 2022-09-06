f2610aa8f9bbf7b05d7711f93b4f413bac3ee95e
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-09-06 20:32:03 +0300

    Create SignIn view and SignInTextAndSecureField

---
+++ AppTheMovieDB/Views/Helpers/SignInTextAndSecureField.swift
@@ -0,0 +1,44 @@
+//
+//  SignInTextAndSecureField.swift
+//  AppTheMovieDB
+//
+//  Created by Вячеслав Гудзенко on 06.09.2022.
+//
+
+import SwiftUI
+
+enum TypeTextField{
+    case textField
+    case secureField
+}
+
+struct SignInTextAndSecureField: View {
+    let strokeColor:UIColor = UIColor(red: 99 / 255, green: 200 / 255, blue: 166 / 255, alpha: 1)
+    @Binding var text:String
+    let placeholderText:String
+    let type:TypeTextField
+    
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.init(strokeColor),lineWidth: 2)
+            if text.isEmpty{
+                Text(placeholderText)
+                    .foregroundColor(.gray)
+                    .font(.title)
+            }
+            switch type {
+            case .textField:
+                TextField("", text: $text)
+                    .padding()
+                    .font(.title)
+                    .foregroundColor(.white)
+            case .secureField:
+                SecureField("", text: $text)
+                    .padding()
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
---
+++ AppTheMovieDB/Views/SignIn.swift
@@ -0,0 +1,60 @@
+//
+//  SignIn.swift
+//  AppTheMovieDB
+//
+//  Created by Вячеслав Гудзенко on 06.09.2022.
+//
+
+import SwiftUI
+
+struct SignIn: View {
+    
+    //@EnvironmentObject var movieFetcher:MovieFetcher
+    @State var username:String = ""
+    @State var password:String = ""
+    
+    var body: some View {
+        ZStack{
+            let backgoundColor:UIColor = UIColor(red: 14 / 255.0, green: 36 / 255.0, blue: 63 / 255.0, alpha: 1)
+            Color.init(backgoundColor).ignoresSafeArea(.all)
+            VStack{
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top,100)
+                
+                SignInTextAndSecureField(text: $username, placeholderText: "Enter your name", type: .textField)
+                    .frame(height:70)
+                    .padding(.top,50)
+                    
+                SignInTextAndSecureField(text: $password, placeholderText: "Enter your password", type: .secureField)
+                    .frame(height:70)
+                    .padding(.top,50)
+                    
+                Button {
+                    print("works")
+                } label: {
+                    let startColor:UIColor = UIColor(red: 99 / 255, green: 200 / 255, blue: 166 / 255, alpha: 1)
+                    let endColor:UIColor = UIColor(red: 82 / 255, green: 179 / 255, blue: 221 / 255, alpha: 1)
+                    
+                    Text("Go")
+                        .frame(width: 180, height: 70)
+                        .background(LinearGradient(colors: [.init(startColor),.init(endColor)], startPoint: .leading, endPoint: .trailing))
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .foregroundColor(.white)
+                        .padding(.top,50)
+                }
+
+                    
+                Spacer()
+                
+            }.padding(.horizontal,30)
+        }
+    }
+}
+
+struct SignIn_Previews: PreviewProvider {
+    static var previews: some View {
+        SignIn()
+    }
+}
