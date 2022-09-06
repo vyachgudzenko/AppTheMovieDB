1691691b4a6578ae0caaa5baeef13f06a795c8bc
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-09-06 03:41:27 +0300

    Create URLConstans

---
+++ AppTheMovieDB/API/URLConstans.swift
@@ -0,0 +1,22 @@
+//
+//  URLConstans.swift
+//  AppTheMovieDB
+//
+//  Created by Вячеслав Гудзенко on 06.09.2022.
+//
+
+import Foundation
+
+struct URLConstans:Codable{
+    var apiKey = "7ddd428c76f6f9fd931cd37916fcc80a"
+    var requestTokenLink:String {
+        "https://api.themoviedb.org/3/authentication/token/new?api_key=\(apiKey)"
+    }
+    var authWithLoginLink:String {
+        "https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=\(apiKey)"
+    }
+    var sessionIdLink:String {
+        "https://api.themoviedb.org/3/authentication/session/new?api_key=\(apiKey)"
+    }
+    
+}
